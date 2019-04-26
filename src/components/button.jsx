b7c7c88172f5e61eec45448102d0d9b9f783451b
Mac <mac@MacdeMac-mini.local>
2019-04-26 17:47:20 +0800

    temp

---
+++ src/components/button.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import { Button } from 'antd-mobile';
+//import 'antd-mobile/lib/button/style/css'
+
+
+class MyButton extends React.Component{
+
+
+
+
+    render(){
+
+
+        return (
+            <Button type='primaty' size="small" inline> button</Button>
+        )
+    }
+}
+
+export default MyButton;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
+import MyButton from './components/button.jsx';
+
 
 console.log("this is index.js")
 
-const content = <div>this is content</div>
+const content = <div>this is content
+
+    <p><MyButton></MyButton></p>
+</div>
 
 const myApp = React.createElement("div",null,content)
 
